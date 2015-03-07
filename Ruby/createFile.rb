5def66b73e5c04f4a1743a8e8f9e5976ca3338cd
DhirManish <manishdhir4@gmail.com>
2015-03-07 16:51:50 +0530

    Reading and Writing file operations

---
+++ Ruby/createFile.rb
@@ -0,0 +1,32 @@
+#  createFile.rb
+#  
+#  Copyright 2015 manishdhir <manishdhir@manishdhir-HP-d530-SFF-DG058A>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+write_file = File.new("ruby.out","w")
+
+write_file.puts("Random Text").to_s
+
+write_file.close
+
+read_from_file = File.read("ruby.out")
+
+puts "Data From File: " + read_from_file
+
