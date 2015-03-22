5dc90a90e505f6ab10153b86604baa25d9c6567c
Manish Dhir <manishdhir4@gmail.com>
2015-03-22 12:41:29 +0530

    concept of interbal and external typecasting

---
+++ C++/TypeCasting.cxx
@@ -0,0 +1,43 @@
+/*
+ * TypeCasting.cxx
+ * 
+ * Copyright 2015 manishdhir <manishdhir@manishdhir-HP-d530-SFF-DG058A>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	//type casting is data conversion -
+	//implicit
+	int a;
+	a = 10.1;
+	cout<<a<<endl;
+	//explicit
+	int b;
+	int c;
+	c = 10;
+	b = a + long(c);
+	cout<<b<<endl;
+	return 0;
+}
+
