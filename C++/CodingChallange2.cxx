b7c27713e0ca4f0bf9195e80987a60bfab5e95e8
Manish Dhir <manishdhir4@gmail.com>
2015-03-23 21:15:24 +0530

    funtional approch for finding evenodd number without using % and /

---
+++ C++/CodingChallange2.cxx
@@ -0,0 +1,53 @@
+/*
+ * CodingChallange2.cxx
+ * 
+ * Copyright 2015 manishdhir <manishdhir@manishdhir-HP-d530-SFF-DG058A>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+bool EvenOdd(int);
+
+bool EvenOdd(int a)
+{
+	while(a>0)
+		a = a - 2;
+		if(a==0)
+			return true;
+		
+		return false;
+
+}
+			
+int main(int argc, char **argv)
+{
+	int n;
+	cout<<"enter given value";
+	cin>>n;
+	
+	 if ( EvenOdd(n) == true)
+        cout<<n<<" is even";
+    else
+        cout<<n<<" is odd";
+        
+	return 0;
+}
+
