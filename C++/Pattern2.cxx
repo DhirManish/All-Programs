2b1c9a4a07bc4403e745996a1c866046e6af7192
Manish Dhir <manishdhir4@gmail.com>
2015-03-23 21:12:17 +0530

    pattern two drawn with easy logic

---
+++ C++/Pattern2.cxx
@@ -0,0 +1,43 @@
+/*
+ * Pattern2.cxx
+ * 
+ * Copyright 2015 manishdhir <manishdhir@manishdhir-HP-d530-SFF-DG058A>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	//outer loops is for rows
+	for(int i=1;i<=3;i++){
+		//inner loop is for colomn
+		for(int j=1;j<=3-i;j++){
+			cout<<' ';
+			}
+			for(int k=1;k<=i;k++){
+				cout<<'*';
+			}
+		cout<<endl;
+	}
+	return 0;
+}
+
