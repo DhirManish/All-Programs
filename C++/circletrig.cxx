2472000df99928f6e59f4cb46b6742bce479cad1
Manish Dhir <manishdhir4@gmail.com>
2015-04-08 23:38:36 +0530

    trignometric circle method implemented

---
+++ C++/circletrig.cxx
@@ -0,0 +1,56 @@
+/*
+ * circletrig.cxx
+ * 
+ * Copyright 2015 manishdhir <manishdhir@manishdhir-HP-d530-SFF-DG058A>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+#include <graphics.h>
+#include <math.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int gd=DETECT,gm,h,k;
+	int x,y,r;
+	h=100;k=100;
+	double theta;
+	r=50;
+	int thetaend=90.0;
+	initgraph(&gd,&gm,NULL);
+	//while(x>xend){
+	for(theta=1.0;theta<thetaend;theta+=0.1){
+		x=r*(cos(theta));
+		y=r*(sin(theta));
+		putpixel(x+h,y+k,4);
+		putpixel(y+h,x+k,4);
+		putpixel(-y+h,x+k,4);
+		putpixel(-x+h,y+k,4);
+		putpixel(-x+h,-y+k,4);
+		putpixel(-y+h,-x+k,4);
+		putpixel(y+h,-x+k,4);
+		putpixel(x+h,-y+k,4);
+	}
+	delay(5000);
+	closegraph();
+	return 0;
+}
+
